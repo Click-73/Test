3f91c57a127a2c7995e847eab493d07147b84056
Andrey Nikolaychuk <60553738+Click-73@users.noreply.github.com>
2021-03-15 03:48:27 +0200

    Add files via upload

--- 4/4/index.js
+++ 4/4/index.js
@@ -1,15 +1,14 @@
 function run() {
-  var a = 0;
+  const a = 0;
 
-  if (a == 1) {
-    return c;
+  if (a === 1) {
+    return a;
   }
 
-  for (let i = 1; i < 10; i++) {
+  for (let i = 1; i < 10; i+= 1) {
     console.log(i);
   }
 
-  return (d = a);
+  return (a);
 }
-eslint-errors.txt
 run();
