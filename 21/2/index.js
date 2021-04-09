1ddbb9525b42fd851e4721be8dd07b73358cb1d9
Andrey Nikolaychuk <60553738+Click-73@users.noreply.github.com>
2021-04-09 12:27:51 +0300

    Add files via upload

---
+++ 21/2/index.js
@@ -0,0 +1,12 @@
+export const getTitleElement = () => {
+  const titleElem = document.querySelector(".title");
+  console.dir(titleElem);
+  return titleElem;
+};
+export const getInputElement = () => {
+  const inputElem = document
+    .querySelector("input")
+    .getAttribute("type", "text");
+  console.dir(inputElem);
+  return inputElem;
+};
